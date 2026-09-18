5d18ec689b895eecf3abbc36a4941fd9c9443ee5
Provectus18 <fiddlebert2020@gmail.com>
2026-09-18 19:31:50 -0400

    Made circle blue and movable

--- main.c
+++ main.c
@@ -3,11 +3,10 @@
 int main(void)
 {
     const int W = 800, H = 450;
-    InitWindow(W, H, "Bouncing Ball");
+    InitWindow(W, H, "Frosh");
     SetTargetFPS(60);
 
     Vector2 pos = { W/2.0f, H/2.0f };
-    Vector2 vel = { 300.0f, 250.0f };
     float radius = 20.0f;
 
     while (!WindowShouldClose())
@@ -16,17 +15,35 @@ int main(void)
 	if (IsKeyPressed(KEY_F)){
 	    ToggleFullscreen();
 	  }
-        // Move
-        pos.x += vel.x * dt;
-        pos.y += vel.y * dt;
+        
+	// Move the ball with Key inputs
+	
+	if (IsKeyDown(KEY_RIGHT)) pos.x += 2.0f;
+        if (IsKeyDown(KEY_LEFT)) pos.x -= 2.0f;
+        if (IsKeyDown(KEY_UP)) pos.y -= 2.0f;
+        if (IsKeyDown(KEY_DOWN)) pos.y += 2.0f;
+	
+        // Prevent the ball from going out of frame, cling to the side
 
-        // Bounce off edges
-        if (pos.x - radius < 0 || pos.x + radius > W) vel.x *= -1;
-        if (pos.y - radius < 0 || pos.y + radius > H) vel.y *= -1;
+	 if (pos.x - radius < 0) {
+        pos.x = radius;
+        }
+        if (pos.x + radius > W) {
+        pos.x = W - radius;
+        }
+        if (pos.y - radius < 0) {
+        pos.y = radius;
+        }
+        if (pos.y + radius > H) {
+        pos.y = H - radius;
+        }
 
-        BeginDrawing();
+        if (pos.x - radius < W)
+	
+	// Draw the circle on the screen
+	BeginDrawing();
             ClearBackground(RAYWHITE);
-            DrawCircleV(pos, radius, RED);
+            DrawCircleV(pos, radius, BLUE);
             DrawText(TextFormat("FPS: %d", GetFPS()), 10, 10, 20, DARKGRAY);
         EndDrawing();
     }
