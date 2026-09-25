3f39e0fd910629d7a9d0710f2a83c3e9ab6d9bd8
Provectus18 <fiddlebert2020@gmail.com>
2026-09-24 23:09:10 -0400

    Removed gravity and jumping, due to change of plans; Generated a cleaned up version of main.c with an llm

--- main.c
+++ main.c
@@ -1,93 +1,83 @@
 #include "raylib.h"
 
-struct Player {
-  Vector2 pos;
-  float radius;
-};
+#define SCREEN_WIDTH  800
+#define SCREEN_HEIGHT 450
+#define MOVE_SPEED    500.0f
 
-  
+typedef struct Player {
+    Vector2 pos;
+    float radius;
+} Player;
+
 int main(void) {
-  const int W = 800, H = 450;
-  InitWindow(W, H, "Frosh");
-  InitAudioDevice();  
-  SetTargetFPS(60);
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Frosh");
+    InitAudioDevice();
+    SetTargetFPS(60);
 
     Music music = LoadMusicStream("pond.wav");
     SetMusicVolume(music, 0.7f);
     PlayMusicStream(music);
-    
+
     Camera2D camera = {
-      .offset = (Vector2){ W/2.0f, H/2.0f },
-      .rotation = 0.0f,
-      .zoom = 1.0f
+        .offset = (Vector2){ SCREEN_WIDTH / 2.0f, SCREEN_HEIGHT / 2.0f },
+        .rotation = 0.0f,
+        .zoom = 1.0f
     };
 
-    struct Player player = {
-      .pos = { 0.0f, -20.0f },
-      .radius = 20.0f
+    Player player = {
+        .pos = { 0.0f, -20.0f },
+        .radius = 15.0f
     };
 
     Vector2 velocity = { 0.0f, 0.0f };
-    const float GRAVITY = 1500.0f;
-    const float MOVE_SPEED = 500.0f;
-    const float JUMP_FORCE = -600.0f;
-    bool grounded = false;
 
+    Rectangle greenBlock = { 0.0f, 0.0f, 100.0f, 100.0f };
 
     while (!WindowShouldClose()) {
-      float dt = GetFrameTime();
-      camera.target = player.pos;
+        float dt = GetFrameTime();
+        camera.target = player.pos;
 
-      if (IsKeyPressed(KEY_F)) ToggleFullscreen();
-      if (IsKeyDown(KEY_RIGHT)) velocity.x = MOVE_SPEED;
-      else if (IsKeyDown(KEY_LEFT)) velocity.x = -MOVE_SPEED;
-      else velocity.x = 0.0f;
+        // Input keys
+        if (IsKeyPressed(KEY_F)) ToggleFullscreen();
 
+        velocity.x = 0.0f;
+        velocity.y = 0.0f;
+        if (IsKeyDown(KEY_RIGHT)) velocity.x =  MOVE_SPEED;
+        if (IsKeyDown(KEY_LEFT))  velocity.x = -MOVE_SPEED;
+        if (IsKeyDown(KEY_UP))    velocity.y = -MOVE_SPEED;
+        if (IsKeyDown(KEY_DOWN))  velocity.y =  MOVE_SPEED;
 
-      velocity.y += GRAVITY * dt;
+        // Movement and collision
+        Vector2 oldPos = player.pos;
 
-      
-      if (IsKeyPressed(KEY_SPACE)) {
-        velocity.y = JUMP_FORCE;
-      }
+        // X axis
+        player.pos.x += velocity.x * dt;
+        if (CheckCollisionCircleRec(player.pos, player.radius, greenBlock)) {
+            player.pos.x = oldPos.x;
+            velocity.x = 0.0f;
+        }
 
-      // Store the old position for collision.
-      Vector2 oldPos = player.pos;
+        // Y axis
+        player.pos.y += velocity.y * dt;
+        if (CheckCollisionCircleRec(player.pos, player.radius, greenBlock)) {
+            player.pos.y = oldPos.y;
+            velocity.y = 0.0f;
+        }
 
-      
-      Rectangle greenBlock = {
-        0.0f, 0.0f,
-        100.0f, 100.0f
-      };
-      
-      // Collision
-      player.pos.x += velocity.x * dt;
-      if (CheckCollisionCircleRec(player.pos, player.radius, greenBlock)) {
-	player.pos.x = oldPos.x;  // Revert the X position when colliding
-	velocity.x = 0.0f;
-      }
+        // Render the game
+        BeginDrawing();
+            ClearBackground(RAYWHITE);
+            BeginMode2D(camera);
+                DrawCircleV(player.pos, player.radius, BLUE);
+                DrawRectangleRec(greenBlock, GREEN);
+            EndMode2D();
+        EndDrawing();
 
-      player.pos.y += velocity.y * dt;
-      if (CheckCollisionCircleRec(player.pos, player.radius, greenBlock)) {
-	if (velocity.y > 0) {
-	  player.pos.y = greenBlock.y - player.radius;
-	  grounded = true;
-	} else {
-	  player.pos.y = greenBlock.y + greenBlock.height + player.radius;
-	}
-	velocity.y = 0.0f;
-      }
-
-      // Display the game
-      BeginDrawing();
-          ClearBackground(RAYWHITE);
-          BeginMode2D(camera); 
-          DrawCircleV(player.pos, player.radius, BLUE);
-          DrawRectangleRec(greenBlock, GREEN);
-          UpdateMusicStream(music);
-      EndDrawing();
+        UpdateMusicStream(music);
     }
 
+    UnloadMusicStream(music);
+    CloseAudioDevice();
     CloseWindow();
     return 0;
 }
